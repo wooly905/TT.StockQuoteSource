0938c01da1814d88d92786eb43ef18c6c723836b
Bruce L <bruceyclee@gmail.com>
2018-04-19 00:32:20 -0700

    update BuildAndTest.cmd for test configuration

--- BuildAndTest.cmd
+++ BuildAndTest.cmd
@@ -4,4 +4,4 @@ dotnet restore --packages .\packages
 
 dotnet build -c Release --no-restore
 
-dotnet test .\test\TT.StockQuoteSource.TestsInNetCore\TT.StockQuoteSource.TestsInNetCore.csproj -v n --no-build --no-restore
+dotnet test .\test\TT.StockQuoteSource.TestsInNetCore\TT.StockQuoteSource.TestsInNetCore.csproj -v n --no-build --no-restore -c Release
